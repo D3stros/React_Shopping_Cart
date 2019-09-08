6b2d758694c883eb0cbbe991eda0b5ff5e64a25a
Destros <patrick.fallmann@hotmail.com>
2019-09-08 17:38:43 +0200

    Binding event handler handleIncrement to the current Counter object via constructor .bind(this)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,41 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: []
+    count: 0
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // Binding event handler handleIncrement to the current Counter object via constructor .bind(this)
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Increment clicked", this);
   }
 
   render() {
-    // {this.state.tags.length === 0 && "Please create a new tag"}
-    // If all operands are truthy JS will return the last operand
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
