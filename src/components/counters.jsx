ba58d0a918cda933f60d99a8f693c2cc3be6a53f
Destros <patrick.fallmann@hotmail.com>
2019-09-09 16:05:22 +0200

    Updating the state and optimizing the code

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,14 +11,22 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       // Getting each counter and map it to a counter component
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
